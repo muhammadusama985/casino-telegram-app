3a57fbb8e4b48293156d48a1c4ec077311372070
muhammad usama <mohammadusama985@gmail.com>
2025-08-15 07:03:31 +0500

    wallet integration updated again

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
 
 createRoot(document.getElementById('root')).render(
 
